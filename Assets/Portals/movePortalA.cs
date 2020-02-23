2543a81cf9390845c153cdd536feb948d6d4996b
Stouki <damien.cochard@epitech.eu>
2020-02-23 19:37:47 +0100

    working teleporter and portalgun, objective is to reach the point

---
+++ Assets/Portals/movePortalA.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movePortalA : MonoBehaviour
+{
+    private GameObject portal;
+    // Start is called before the first frame update
+    void Start()
+    {
+        portal = GameObject.Find("Portal A");
+        portal.transform.position = transform.position;
+        portal.transform.rotation = transform.rotation;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
